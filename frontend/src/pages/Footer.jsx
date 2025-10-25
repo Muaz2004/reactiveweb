38b593724233d01ca5cf05776fd4bd7b6a2a919b
Muaz2004 <muazindris64@gmail.com>
2025-10-25 10:51:21 +0300

    footer styled

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -1,21 +1,13 @@
-// src/pages/Footer.jsx
-import { Link } from 'react-router-dom';
+// src/components/Footer.jsx
+import React from 'react';
 import './Footer.css';
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-links">
-        <Link to="/">Home</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/contact">Contact Us</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/donate">Donation</Link>
-        <Link to="/volunteer">Volunteer</Link>
-      </div>
-      <div className="footer-rights">
-        &copy; {new Date().getFullYear()} Genet Engenag Organization. All rights reserved.
-      </div>
+      <span className="footer-text">
+        &copy; {new Date().getFullYear()} Genet Engenag Organization. All rights reserved. Developed by Muaz
+      </span>
     </footer>
   );
 }
